125397be83c54bccc724f492475b4d489eddc7bc
Thiagooms <thiagopaulista50@gmail.com>
2026-03-09 13:43:33 -0300

    Repository created, challenge begins

---
+++ src/main/java/com/desafios/custom_loans/CustomLoansApplication.java
@@ -0,0 +1,13 @@
+package com.desafios.custom_loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomLoansApplication.class, args);
+	}
+
+}
